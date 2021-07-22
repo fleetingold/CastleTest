5358cc7b896b95a7194ba9ff008277a5eec9eae6
fleetingold <1161851523@qq.com>
2021-07-22 11:04:03 +0800

    add Visual Studio Project CastleTest

---
+++ CastleTest/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CastleTest
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}

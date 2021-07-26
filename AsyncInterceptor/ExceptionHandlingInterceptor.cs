7aa1efb73e7c3e9bc820d992487e376a4d7912bb
fleetingold <1161851523@qq.com>
2021-07-26 14:06:50 +0800

    Inherited AsyncInterceptorBase

---
+++ AsyncInterceptor/ExceptionHandlingInterceptor.cs
@@ -0,0 +1,41 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncInterceptor
+{
+    public class ExceptionHandlingInterceptor : AsyncInterceptorBase
+    {
+        protected override async Task InterceptAsync(IInvocation invocation, IInvocationProceedInfo proceedInfo, Func<IInvocation, IInvocationProceedInfo, Task> proceed)
+        {
+            try
+            {
+                // Cannot simply return the the task, as any exceptions would not be caught below.
+                await proceed(invocation, proceedInfo).ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError($"Error calling {invocation.Method.Name}.", ex);
+                throw;
+            }
+        }
+
+        protected override async Task<TResult> InterceptAsync<TResult>(IInvocation invocation, IInvocationProceedInfo proceedInfo, Func<IInvocation, IInvocationProceedInfo, Task<TResult>> proceed)
+        {
+            try
+            {
+                // Cannot simply return the the task, as any exceptions would not be caught below.
+                return await proceed(invocation, proceedInfo).ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError($"Error calling {invocation.Method.Name}.", ex);
+                throw;
+            }
+        }
+    }
+}
---
+++ AsyncInterceptor/InheritedAsyncInterceptorBase.cs
@@ -0,0 +1,19 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Threading.Tasks;
+
+namespace AsyncInterceptor
+{
+    public class InheritedAsyncInterceptorBase : AsyncInterceptorBase
+    {
+        protected override Task InterceptAsync(IInvocation invocation, IInvocationProceedInfo proceedInfo, Func<IInvocation, IInvocationProceedInfo, Task> proceed)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Task<TResult> InterceptAsync<TResult>(IInvocation invocation, IInvocationProceedInfo proceedInfo, Func<IInvocation, IInvocationProceedInfo, Task<TResult>> proceed)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

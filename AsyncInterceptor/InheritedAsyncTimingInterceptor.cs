094ec8afca0cc4b117bdde3e570a7d2aad5fba0c
fleetingold <1161851523@qq.com>
2021-07-26 15:50:02 +0800

    add InheritedAsyncTimingInterceptor

---
+++ AsyncInterceptor/InheritedAsyncTimingInterceptor.cs
@@ -0,0 +1,23 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncInterceptor
+{
+    public class InheritedAsyncTimingInterceptor : AsyncTimingInterceptor
+    {
+        protected override void CompletedTiming(IInvocation invocation, Stopwatch stopwatch)
+        {
+            Trace.WriteLine($"{invocation.Method.Name}:CompletedTiming:{stopwatch.Elapsed:g}");
+        }
+
+        protected override void StartingTiming(IInvocation invocation)
+        {
+            Trace.WriteLine($"{invocation.Method.Name}:StartingTiming");
+        }
+    }
+}

ac3f89f5b6b3fa1e75fa724dea8e59116762ed94
fleetingold <1161851523@qq.com>
2021-07-26 14:34:58 +0800

    Inherited ProcessingAsyncInterceptor

---
+++ AsyncInterceptor/ExtendProcessingAsyncInterceptor.cs
@@ -0,0 +1,20 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Diagnostics;
+
+namespace AsyncInterceptor
+{
+    public class ExtendProcessingAsyncInterceptor : ProcessingAsyncInterceptor<string>
+    {
+        protected override string StartingInvocation(IInvocation invocation)
+        {
+            return $"{invocation.Method.Name}:StartingInvocation:{DateTime.UtcNow:O}";
+        }
+
+        protected override void CompletedInvocation(IInvocation invocation, string state)
+        {
+            Trace.WriteLine(state);
+            Trace.WriteLine($"{invocation.Method.Name}:CompletedInvocation:{DateTime.UtcNow:O}");
+        }
+    }
+}
